e581f5ca540c437bca15cfd2e9b4053be8c8e3f5
cnpoe <cnpoek@gmail.com>
2017-06-02 00:28:57 -0500

    Ejemplo de insertar datos en vistas

--- src/main/java/com/udemy/controller/ExampleController.java
+++ src/main/java/com/udemy/controller/ExampleController.java
@@ -1,6 +1,7 @@
 package com.udemy.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,11 +14,14 @@ public class ExampleController {
 	public static final String EXAMPLE_VIEW = "example"; 
 	//@RequestMapping(value="/exampleString", method=RequestMethod.GET)
 	@GetMapping("/exampleString")
-	public String exampleString(){
+	public String exampleString(Model model){
+		model.addAttribute( "name", "Luis" );
 		return EXAMPLE_VIEW;
 	}
 	@RequestMapping(value="/exampleMAV", method=RequestMethod.GET)
 	public ModelAndView exampleMAV(){
-		return new ModelAndView(EXAMPLE_VIEW);
+		ModelAndView mav = new ModelAndView(EXAMPLE_VIEW);
+		mav.addObject("name", "Alberto");
+		return mav;
 	}
 }
