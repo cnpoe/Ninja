49c0e2141701638a939f5ba193b7f20588de4d79
cnpoe <cnpoek@gmail.com>
2017-06-08 23:32:11 -0500

    GET Path Variable Ejemplo

--- src/main/java/com/udemy/controller/Example2Controller.java
+++ src/main/java/com/udemy/controller/Example2Controller.java
@@ -2,6 +2,7 @@ package com.udemy.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -16,6 +17,13 @@ public class Example2Controller {
 	public ModelAndView request1( @RequestParam( name="nm" , required=false , defaultValue="Usuario" ) String name ){
 		ModelAndView mav = new ModelAndView( EXAMPLE2_VIEW );
 		mav.addObject( "nm_in_model" , name );
+		return mav;
+	}
+	
+	@GetMapping("/request2/{nm}")
+	public ModelAndView request2( @PathVariable( "nm" ) String nombre ){
+		ModelAndView mav = new ModelAndView( EXAMPLE2_VIEW );
+		mav.addObject( "nm_in_model" , nombre );
 		return mav;
 	}
 }
