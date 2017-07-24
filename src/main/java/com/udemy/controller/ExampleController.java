badfd61beb9a0a5e583fbfbd913827cb0cde6bc7
cnpoe <cnpoek@gmail.com>
2017-07-23 23:49:41 -0500

    Ejemplo de componente en Spring

--- src/main/java/com/udemy/controller/ExampleController.java
+++ src/main/java/com/udemy/controller/ExampleController.java
@@ -3,6 +3,8 @@ package com.udemy.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.udemy.component.ExampleComponent;
 import com.udemy.model.Person;
 
 
@@ -18,10 +21,19 @@ import com.udemy.model.Person;
 @RequestMapping("/example")
 public class ExampleController {
 	
+	/*
+	 * Autowired caraga el componente en la memoria
+	 * Qualifier indica el nombre del compoenente
+	 * */
+	@Autowired
+	@Qualifier("exampleComponent")
+	private ExampleComponent exampleComponent;
+	
 	public static final String EXAMPLE_VIEW = "example"; 
 	//@RequestMapping(value="/exampleString", method=RequestMethod.GET)
 	@GetMapping("/exampleString")
 	public String exampleString(Model model){
+		exampleComponent.sayHello();
 		model.addAttribute( "people", getPeople() );
 		return EXAMPLE_VIEW;
 	}
